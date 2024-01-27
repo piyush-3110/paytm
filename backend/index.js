8f99dc207af3de5b37db0dffc0957ea17e347c6a
Piyush-Das <piyushdas3110@gmail.com>
2024-01-27 23:35:00 +0530

    done till step4

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
-
-
+const rootRouter = require("./routes/index");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+app.use("/api/v1", rootRouter);
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
